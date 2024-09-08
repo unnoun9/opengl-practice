487201180d564dd093f85dba62051f090fca7338
sad <sad@sad>
2024-09-09 01:15:00 +0500

    ...

--- build.sh
+++ build.sh
@@ -1,5 +1,3 @@
-mkdir -p bin
+g++ -std=c++17 -I$(pwd) -o Application src/*.cpp src/glad.c -lglfw3
 
-g++ -std=c++17 -I$(pwd) -o bin/Application src/*.cpp src/glad.c -lglfw3
-
-./bin/Application
+./Application
